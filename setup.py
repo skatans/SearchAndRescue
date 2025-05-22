af9ceff5887cf6260df9eb0d15eabb520b042c16
Sandra <svekho@utu.fi>
2025-05-22 16:37:13 +0300

    turtlebot stuff

--- setup.py
+++ setup.py
@@ -1,28 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'my_package'
-data_files = []
-data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
-data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
-data_files.append(('share/' + package_name + '/worlds', ['worlds/my_world.wbt']))
-data_files.append(('share/' + package_name + '/resource', ['resource/my_robot.urdf']))
-data_files.append(('share/' + package_name, ['package.xml']))
+package_name = 'turtlebot_controller'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=data_files,
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='user',
-    maintainer_email='user.name@mail.com',
+    maintainer='sandra',
+    maintainer_email='svekho@utu.fi',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'my_robot_driver = my_package.my_robot_driver:main',
+            'simple_controller = turtlebot_controller.simple_controller:main',
         ],
     },
 )
