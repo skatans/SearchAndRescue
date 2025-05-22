6355ddf1256197a254664acf1665e6ec11924808
Sandra <svekho@utu.fi>
2025-05-22 22:40:39 +0300

    improved turtlebot in world

--- turtlebot_controller/simple_controller.py
+++ turtlebot_controller/simple_controller.py
@@ -1,19 +1,19 @@
 # turtlebot_controller/simple_controller.py
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import Twist
 
 class SimpleController(Node):
     def __init__(self):
         super().__init__('simple_controller')
-        self.publisher_ = self.create_publisher(TwistStamped, '/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
-        msg = TwistStamped()
-        msg.twist.linear.x = 0.2
-        msg.twist.angular.z = 0.3
+        msg = Twist()
+        msg.linear.x = 0.2
+        msg.angular.z = 0.3
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing cmd_vel: linear=0.2, angular=0.3')
 
